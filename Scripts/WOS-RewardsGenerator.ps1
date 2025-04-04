6644d48fcabdf43fb926be64845cc758ee2473c6
Derek-Hartman <57258132+Derek-Hartman@users.noreply.github.com>
2025-04-04 14:41:10 -0400

    Update WOS-RewardsGenerator.ps1
    Added option for lower item reward count.

--- Scripts/WOS-RewardsGenerator.ps1
+++ Scripts/WOS-RewardsGenerator.ps1
@@ -1,4 +1,5 @@
-﻿$Total = Read-Host "How many to give out?"
+#Script﻿
+$Total=Read-Host "How many to give out?"
 
 $R4s = Read-Host "How many R4s"
 
@@ -6,31 +7,62 @@ $R3s = Read-Host "How many R3s"
 
 $R2s = Read-Host "How many R2s"
 
-$Int = 1
-
-$NumberTest = $False
-
 $LowerSum = $Total - 10
 
-While (($NumberTest -eq $False) -and ($Int -lt 120)) {
+Write-Host "Menu:"
+Write-Host "1. Give limit 10"
+Write-Host "2. Give limit 4"
+$choice = Read-Host "Enter your choice"
 
-    $R4sNum = Get-Random -Minimum 6 -Maximum 9
-    $R3sNum = Get-Random -Minimum 3 -Maximum 7
-    $R2sNum = Get-Random -Minimum 1 -Maximum 5
+switch ($choice) {
+    "1" {
+        $Int = 1
+        $NumberTest = $False
+        While (($NumberTest -eq $False) -and ($Int -lt 120)) {
+            $R4sNum = Get-Random -Minimum 6 -Maximum 9
+            $R3sNum = Get-Random -Minimum 3 -Maximum 7
+            $R2sNum = Get-Random -Minimum 1 -Maximum 5
 
-    $R4sTotal = [int]$R4s * [int]$R4sNum
-    $R3sTotal = [int]$R3s * [int]$R3sNum
-    $R2sTotal = [int]$R2s * [int]$R2sNum
+            $R4sTotal = [int]$R4s * [int]$R4sNum
+            $R3sTotal = [int]$R3s * [int]$R3sNum
+            $R2sTotal = [int]$R2s * [int]$R2sNum
 
-    $Sum = $R4sTotal + $R3sTotal + $R2sTotal
+            $Sum = $R4sTotal + $R3sTotal + $R2sTotal
 
-    $Int++
+            $Int++
 
-    If ($Sum -lt $Total -and $Sum -gt $LowerSum) {
-        Write-Host "The Sum is $Sum"
-        Write-Host "R4s get $R4sNum"
-        Write-Host "R3s get $R3sNum"
-        Write-Host "R2s get $R2sNum"
-        $NumberTest = $True
+            If ($Sum -lt $Total -and $Sum -gt $LowerSum) {
+                Write-Host "The Sum is $Sum"
+                Write-Host "R4s get $R4sNum"
+                Write-Host "R3s get $R3sNum"
+                Write-Host "R2s get $R2sNum"
+                $NumberTest = $True
+            }
+        }
+    }
+    "2" {
+        $Int = 1
+        $NumberTest = $False
+        While (($NumberTest -eq $False) -and ($Int -lt 120)) {
+            $R4sNum = Get-Random -Minimum 1 -Maximum 3
+            $R3sNum = Get-Random -Minimum 1 -Maximum 2
+            $R2sNum = Get-Random -Minimum 1 -Maximum 2
+
+            $R4sTotal = [int]$R4s * [int]$R4sNum
+            $R3sTotal = [int]$R3s * [int]$R3sNum
+            $R2sTotal = [int]$R2s * [int]$R2sNum
+
+            $Sum = $R4sTotal + $R3sTotal + $R2sTotal
+            Write-Host $Sum
+            $Int++
+
+            If ($Sum -lt $Total -and $Sum -gt $LowerSum) {
+                Write-Host "The Sum is $Sum"
+                Write-Host "R4s get $R4sNum"
+                Write-Host "R3s get $R3sNum"
+                Write-Host "R2s get $R2sNum"
+                $NumberTest = $True
+            }
+        }
     }
 }
