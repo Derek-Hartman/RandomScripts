0f9d11a1db15728f7509dbe6ca91f08d93028861
Derek-Hartman <57258132+Derek-Hartman@users.noreply.github.com>
2025-07-04 22:38:32 -0400

    Update WOS-RewardsGenerator.ps1
    Bug fix with totals

--- Scripts/WOS-RewardsGenerator.ps1
+++ Scripts/WOS-RewardsGenerator.ps1
@@ -1,4 +1,4 @@
-﻿#Script﻿
+#Script﻿
 $Total=Read-Host "How many to give out?"
 
 $R4s = Read-Host "How many R4s"
@@ -7,12 +7,13 @@ $R3s = Read-Host "How many R3s"
 
 $R2s = Read-Host "How many R2s"
 
-$LowerSum = $Total - 20
+$LowerSum = $Total - 50
 
 Write-Host "Menu:"
 Write-Host "1. Give limit 10"
 Write-Host "2. Give limit 4"
 Write-Host "3. Give limit 15"
+Write-Host "4. Give limit 20"
 $choice = Read-Host "Enter your choice"
 
 switch ($choice) {
@@ -83,7 +84,34 @@ switch ($choice) {
             $R3sTotal = [int]$R3s * [int]$R3sNum
             $R2sTotal = [int]$R2s * [int]$R2sNum
 
-            $Sum = $R4sTotal + $R3sTotal + $R2sTotal5
+            $Sum = $R4sTotal + $R3sTotal + $R2sTotal
+            $Int++
+
+            If ($Sum -lt $Total -and $Sum -gt $LowerSum) {
+                Write-Host "The Sum is $Sum"
+                Write-Host "R4s get $R4sNum"
+                Write-Host "R3s get $R3sNum"
+                Write-Host "R2s get $R2sNum"
+                $NumberTest = $True
+            }
+            Else {
+                #Write-Host $Sum
+            }
+        }
+    }
+    "4" {
+        $Int = 1
+        $NumberTest = $False
+        While (($NumberTest -eq $False) -and ($Int -lt 120)) {
+            $R4sNum = Get-Random -Minimum 12 -Maximum 20
+            $R3sNum = Get-Random -Minimum 8 -Maximum 16
+            $R2sNum = Get-Random -Minimum 2 -Maximum 10
+
+            $R4sTotal = [int]$R4s * [int]$R4sNum
+            $R3sTotal = [int]$R3s * [int]$R3sNum
+            $R2sTotal = [int]$R2s * [int]$R2sNum
+
+            $Sum = $R4sTotal + $R3sTotal + $R2sTotal
             $Int++
 
             If ($Sum -lt $Total -and $Sum -gt $LowerSum) {
